fa4606b6a932e54c05d8d16c3c433fc1dc11aa5c
annlvova <annlvova.7th@gmail.com>
2022-04-14 21:10:40 +0700

    add arrays inside page content

--- index.php
+++ index.php
@@ -1,44 +1,7 @@
 <?php
 $show_complete_tasks = rand(0, 1);
-$projects = ["Входящие", "Учеба", "Работа", "Домашние дела", "Авто"];
-$tasks = [
-    [
-        'name' => 'Собеседование в IT компании',
-        'date' => '01.12.2019',
-        'project' => 'Работа',
-        'done' => false 
-    ],
-    [
-        'name' => 'Выполнить тестовое задание',
-        'date' => '25.12.2019',
-        'project' => 'Работа',
-        'done' => false  
-    ],
-    [
-        'name' => 'Сделать задание первого раздела',
-        'date' => '21.12.2019',
-        'project' => 'Учеба',
-        'done' => true      
-    ],
-    [
-        'name' => 'Встреча с другом',
-        'date' => '22.12.2019',
-        'project' => 'Входящие',
-        'done' => false  
-    ],
-    [
-        'name' => 'Купить корм для кота',
-        'date' => null,
-        'project' => 'Домашние дела',
-        'done' => false
-    ],
-    [
-        'name' => 'Заказать пиццу',
-        'date' => null,
-        'project' => 'Домашние дела',
-        'done' => false
-    ]
-    ];
+
+
 
 function project_count($tasks, $project){  
     $count = 0;    
@@ -49,9 +12,49 @@ function project_count($tasks, $project){
         }
         return $count;
     }
-$page_content = function include_template('main.php', [
-    'projects' => $projects], 
-    ['tasks' => $tasks]);
+$page_content = function include_template('main.php',
+    $projects = ["Входящие", "Учеба", "Работа", "Домашние дела", "Авто"], 
+    $tasks = [
+        [
+            'name' => 'Собеседование в IT компании',
+            'date' => '01.12.2019',
+            'project' => 'Работа',
+            'done' => false 
+        ],
+        [
+            'name' => 'Выполнить тестовое задание',
+            'date' => '25.12.2019',
+            'project' => 'Работа',
+            'done' => false  
+        ],
+        [
+            'name' => 'Сделать задание первого раздела',
+            'date' => '21.12.2019',
+            'project' => 'Учеба',
+            'done' => true      
+        ],
+        [
+            'name' => 'Встреча с другом',
+            'date' => '22.12.2019',
+            'project' => 'Входящие',
+            'done' => false  
+        ],
+        [
+            'name' => 'Купить корм для кота',
+            'date' => null,
+            'project' => 'Домашние дела',
+            'done' => false
+        ],
+        [
+            'name' => 'Заказать пиццу',
+            'date' => null,
+            'project' => 'Домашние дела',
+            'done' => false
+        ]
+        ]
+) {
+    $page_content = 'templates/main.php'
+}
 $layout_content = function include_template('layout.php', [
 	'content' => $page_content,
 	'title' => 'Дела в порядке'
