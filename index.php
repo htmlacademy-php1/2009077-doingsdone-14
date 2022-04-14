dab8f297ca8a2807b02a276ad152f4d6cf3a02e4
annlvova <annlvova.7th@gmail.com>
2022-04-14 21:58:45 +0700

    requiring helpers and connecting templates

--- index.php
+++ index.php
@@ -1,5 +1,47 @@
 <?php
+require_once('helpers.php');
+
 $show_complete_tasks = rand(0, 1);
+
+$projects = ["Входящие", "Учеба", "Работа", "Домашние дела", "Авто"], 
+$tasks = [
+    [
+        'name' => 'Собеседование в IT компании',
+        'date' => '01.12.2019',
+        'project' => 'Работа',
+        'done' => false 
+    ],
+    [
+        'name' => 'Выполнить тестовое задание',
+        'date' => '25.12.2019',
+        'project' => 'Работа',
+        'done' => false  
+    ],
+    [
+        'name' => 'Сделать задание первого раздела',
+        'date' => '21.12.2019',
+        'project' => 'Учеба',
+        'done' => true      
+    ],
+    [
+        'name' => 'Встреча с другом',
+        'date' => '22.12.2019',
+        'project' => 'Входящие',
+        'done' => false  
+    ],
+    [
+        'name' => 'Купить корм для кота',
+        'date' => null,
+        'project' => 'Домашние дела',
+        'done' => false
+    ],
+    [
+        'name' => 'Заказать пиццу',
+        'date' => null,
+        'project' => 'Домашние дела',
+        'done' => false
+    ]
+]
 
 function project_count($tasks, $project){  
     $count = 0;    
@@ -10,69 +52,13 @@ function project_count($tasks, $project){
         }
         return $count;
     }
-    
-function include_template(
-    'templates/main.php',
-    $projects = ["Входящие", "Учеба", "Работа", "Домашние дела", "Авто"], 
-    $tasks = [
-        [
-            'name' => 'Собеседование в IT компании',
-            'date' => '01.12.2019',
-            'project' => 'Работа',
-            'done' => false 
-        ],
-        [
-            'name' => 'Выполнить тестовое задание',
-            'date' => '25.12.2019',
-            'project' => 'Работа',
-            'done' => false  
-        ],
-        [
-            'name' => 'Сделать задание первого раздела',
-            'date' => '21.12.2019',
-            'project' => 'Учеба',
-            'done' => true      
-        ],
-        [
-            'name' => 'Встреча с другом',
-            'date' => '22.12.2019',
-            'project' => 'Входящие',
-            'done' => false  
-        ],
-        [
-            'name' => 'Купить корм для кота',
-            'date' => null,
-            'project' => 'Домашние дела',
-            'done' => false
-        ],
-        [
-            'name' => 'Заказать пиццу',
-            'date' => null,
-            'project' => 'Домашние дела',
-            'done' => false
-        ]
-        ]
-) {
-    $page_content = 'templates/main.php';
-    $result = 'page_content'
 
-    if (!is_readable($page_content)) {
-        return $result;
-    }
-
-    ob_start();
-    extract($projects, $tasks);
-    require $page_content;
-
-    $result = ob_get_clean();
-
-    return $result;
-}
-function include_template('templates/layout.php', [
+$page_content = include_template('main.php', ['projects' => $projects], ['tasks' => $tasks]);
+$layout_content = include_template('layout.php', [
 	'content' => $page_content,
-	'title' => 'Дела в порядке',
-    'user-menu__data' => 'Константин'
-    ]);
-print($layout_content);
+	'user-menu__data' => 'Константин',
+	'title' => 'Дела в порядке'
+]);
 
+print($layout_content);
 ?>
