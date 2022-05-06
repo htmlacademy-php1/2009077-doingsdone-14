c7c69bf83a6466c0bb5dace22dcb8c36fe644854
annlvova <annlvova.7th@gmail.com>
2022-05-06 21:24:59 +0700

    delete email

--- queries.sql
+++ queries.sql
@@ -1,5 +1,5 @@
-INSERT INTO users SET created_at = '20.03.2018', email = 'email_konstantin@gmail.com', name = 'Константин', password = 'pass_konstantin';
-INSERT INTO users SET created_at = '07.11.2016', email = 'email_anna@gmail.com', name = 'Анна', password = 'pass_anna';
+INSERT INTO users SET created_at = '20.03.2018', email = 'em_konstantin@gmail.com', name = 'Константин', password = 'pass_konstantin';
+INSERT INTO users SET created_at = '07.11.2016', email = 'em_anna@gmail.com', name = 'Анна', password = 'pass_anna';
 
 INSERT INTO projects (name, user_id) VALUES ('Входящие', '2');
 INSERT INTO projects (name, user_id) VALUES ('Учеба', '2');
@@ -13,8 +13,11 @@ VALUES
     ('02.02.2022', 'Выполнить тестовое задание', 'file', '20.04.2022', '2', '3'),
     ('07.11.2019', 'Сделать задание первого раздела', 'file', '21.12.2019', '1', '2' ),
     ('07.11.2019', 'Встреча с другом', 'file', '22.12.2019', '1', '1' ),
-    ('07.11.2019', 'Купить корм для кота', null, null, '1', '4' ),
-    ('07.11.2019', 'Заказать пиццу', null, null, '1', '4' );
+    ('07.11.2019', 'Купить корм для кота', null, null, '1', '4'),
+    ('07.11.2019', 'Заказать пиццу', null, null, '1', '4');
+
+SELECT user_id FROME projects WHERE user_id = '1';
+SELECT project_id FROME tasks WHERE project_id = '4';
 
 
 
