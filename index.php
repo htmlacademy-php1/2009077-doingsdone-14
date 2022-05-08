76bb6a86cb8c1e844289cc40c6dbd0ab78d728ea
annlvova <annlvova.7th@gmail.com>
2022-05-08 13:19:03 +0700

    connection and reading

--- index.php
+++ index.php
@@ -5,6 +5,32 @@ ini_set('display_startup_errors', 1);
 
 require_once('helpers.php');
 
+$con = mysqli_connect("localhost", "Анна", "pass_anna", "doingsdone");
+mysqli_set_charset($con, "utf8");
+    if ($con === false) {
+       print("Ошибка подключения: " . mysqli_connect_error());
+       } else {
+        $sql = "SELECT name FROM projects WHERE user_id = 2";
+        $result = mysqli_query($con, $sql);
+        $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+            foreach ($rows as $row) {
+                print("Проекты:" . $row['name']);
+                }
+    }
+
+    $con = mysqli_connect("localhost", "Анна", "pass_anna", "doingsdone");
+mysqli_set_charset($con, "utf8");
+    if ($con === false) {
+       print("Ошибка подключения: " . mysqli_connect_error());
+       } else {
+        $sql = "SELECT name FROM tasks WHERE user_id = 2";
+        $result = mysqli_query($con, $sql);
+        $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+            foreach ($rows as $row) {
+                print("Задачи:" . $row['name']);
+                }
+    }
+
 $show_complete_tasks = rand(0, 1);
 
 $projects = ["Входящие", "Учеба", "Работа", "Домашние дела", "Авто"]; 
