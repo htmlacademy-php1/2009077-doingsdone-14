1055afe88f273db3e8c6f672a95f05a3ea9adf5a
annlvova <annlvova.7th@gmail.com>
2022-05-10 14:13:45 +0700

    with tasks

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ mysqli_set_charset($con, "utf8");
 if ($con === false) {
     print("Ошибка подключения: " . mysqli_connect_error());
 } else {
-    $sql = "SELECT name FROM projects WHERE user_id = 2";
+    $sql = "SELECT name FROM projects, tasks WHERE user_id = 2";
     $result = mysqli_query($con, $sql);
     $rows = mysqli_fetch_assoc($result);
 } 
