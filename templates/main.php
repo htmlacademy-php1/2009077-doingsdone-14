f4adf67ec032685dfcf8680f081a2fcde67e93ae
annlvova <annlvova.7th@gmail.com>
2022-05-17 22:21:06 +0700

    project -id

--- templates/main.php
+++ templates/main.php
@@ -7,7 +7,7 @@
                         <?php foreach ($projects as $project): ?>
                         <li class="main-navigation__list-item">
                             <a class="main-navigation__list-item-link" href="#"><?= htmlspecialchars($project['name']); ?></a>
-                            <span class="main-navigation__list-item-count"><?=  project_count($tasks, $project['id']); ?></span>
+                            <span class="main-navigation__list-item-count"><?=  project_count($tasks, $project); ?></span>
                         </li>
                         <?php endforeach; ?>
                     </ul>
