5bea4796c11d45bd1377a262c84e755442be8cf6
annlvova <annlvova.7th@gmail.com>
2022-05-22 22:28:48 +0700

    href

--- templates/main.php
+++ templates/main.php
@@ -6,7 +6,7 @@
                     <ul class="main-navigation__list">
                         <?php foreach ($projects as $project): ?>
                         <li class="main-navigation__list-item">
-                            <a class="main-navigation__list-item-link" href="index.php?project_id=1&name"><?= htmlspecialchars($project['name']); ?></a>
+                            <a class="main-navigation__list-item-link" href="index.php?project_id= <?= $project['id']; ?><?= htmlspecialchars($project['name']); ?></a>
                             <span class="main-navigation__list-item-count"><?=  project_count($tasks, $project); ?></span>
                         </li>
                         <?php endforeach; ?>
