694e78ce2811b5f1486c11a9ed985ce01cbd1131
annlvova <annlvova.7th@gmail.com>
2022-05-25 17:42:35 +0700

    var_dump($task['done']) again

--- templates/main.php
+++ templates/main.php
@@ -44,9 +44,7 @@
 
                 <table class="tasks">
                     <?php foreach ($tasks as $task): ?>
-                        <?php if ($task['done'] === false || $show_complete_tasks === 1): ?> 
-                            var_dump($task['done']);
-                            exit();
+                        <?php if ($task['done'] === false || $show_complete_tasks === 1): var_dump($task['done']); exit(); ?> 
                             <tr class="tasks__item task <?= $task['done'] === true ? 'task--completed' : '' ?> <?= is_soon_expire(date('d-m-Y H:i:s'), $task['end_date']) ? 'task--important' : '' ?>">
                     
                                 <td class="task__select">
