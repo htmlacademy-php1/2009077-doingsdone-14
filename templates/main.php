9f572a366b99b25b33a5b6fa0d99a5ef318e4c40
annlvova <annlvova.7th@gmail.com>
2022-05-25 22:21:12 +0700

    - var_dump

--- templates/main.php
+++ templates/main.php
@@ -45,7 +45,7 @@
                 <table class="tasks">
                     <?php foreach ($tasks as $task): ?>
                         <?php if ((bool)$task['done'] === false || $show_complete_tasks === 1): ?> 
-                            <tr class="tasks__item task <?= $task['done'] === true ? 'task--completed' : '' ?> <?= is_soon_expire(date('d-m-Y H:i:s'), $task['end_date']) var_dump($tasks['end_date']); exit(); ? 'task--important' : '' ?>">
+                            <tr class="tasks__item task <?= $task['done'] === true ? 'task--completed' : '' ?> <?= is_soon_expire(date('d-m-Y H:i:s'), $task['end_date']) ? 'task--important' : '' ?>">
                     
                                 <td class="task__select">
                                     <label class="checkbox task__checkbox">
