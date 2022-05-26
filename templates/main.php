89fc7d483817ba08bd9f20c95aa883b9254b8c0b
annlvova <annlvova.7th@gmail.com>
2022-05-26 22:59:09 +0700

    (bool) done

--- templates/main.php
+++ templates/main.php
@@ -45,11 +45,11 @@
                 <table class="tasks">
                     <?php foreach ($tasks as $task): ?>
                         <?php if ((bool)$task['done'] === false || $show_complete_tasks === 1): ?> 
-                            <tr class="tasks__item task <?= (bool)$task['done'] === true ? 'task--completed' : '' ?> <?= is_soon_expire(date('d-m-Y H:i:s'), (bool)$task['end_date']) ? 'task--important' : '' ?>">
+                            <tr class="tasks__item task <?= (bool)$task['done'] === true ? 'task--completed' : '' ?> <?= is_soon_expire(date('d-m-Y H:i:s'), $task['end_date']) ? 'task--important' : '' ?>">
                     
                                 <td class="task__select">
                                     <label class="checkbox task__checkbox">
-                                        <input class="checkbox__input visually-hidden task__checkbox" type="checkbox" value="1" <?= $task['done'] === true ? 'checked' : '' ?>>
+                                        <input class="checkbox__input visually-hidden task__checkbox" type="checkbox" value="1" <?= (bool)$task['done'] === true ? 'checked' : '' ?>>
                                         <span class="checkbox__text"><?= htmlspecialchars($task['name']); ?></span>
                                     </label>
                                 </td>
